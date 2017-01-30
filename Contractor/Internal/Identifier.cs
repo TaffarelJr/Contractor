39512fe280c4c4cd63960476bbf03556f4b1e916
RJ Hollberg <rj.hollberg@outlook.com>
2017-01-30 01:09:58 -0700

    Add Identifier class. This immutable type identifies a type to be resolved by the container, along with an optional label that can be used to differentiate between multiple registrations of the same type. Also adds the ValueTuple package, which is part of the C#7.0 spec but is not included by default yet in Visual Studio 2017 RC. I explored using a Tuple for the identifier, but decided a concrete type would allow me to optimize performance and avoid repetitive declarations.

---
+++ Contractor/Internal/Identifier.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Contractor.Internal
+{
+    /// <summary>
+    /// Identifies a type to be resolved by the container.
+    /// </summary>
+    public class Identifier
+    {
+        private readonly Type _typeToResolve;
+        private readonly string _label;
+
+        /// <summary>
+        /// Initializes a new identifier for the given type.
+        /// </summary>
+        /// <param name="typeToResolve">The type to be resolved by the container.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="typeToResolve"/> is <b>null</b>.</exception>
+        public Identifier(Type typeToResolve)
+            : this(typeToResolve, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new identifier for the given type and label.
+        /// </summary>
+        /// <param name="typeToResolve">The type to be resolved by the container.</param>
+        /// <param name="label">An optional label that differentiates indentifiers that have the same type to be resolved.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="typeToResolve"/> is <b>null</b>.</exception>
+        public Identifier(Type typeToResolve, string label)
+        {
+            _typeToResolve = typeToResolve ?? throw new ArgumentNullException(nameof(typeToResolve));
+            _label = GetTrimmedValueOrNull(label);
+        }
+
+        /// <summary>
+        /// Gets the type to be resolved by the container.
+        /// </summary>
+        public Type TypeToResolve { get => _typeToResolve; }
+
+        /// <summary>
+        /// Gets the optional label for the type to be resolved.
+        /// </summary>
+        public string Label { get => _label; }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal to each other.
+        /// </summary>
+        /// <param name="left">The first object to be compared.</param>
+        /// <param name="right">The second object to be compared.</param>
+        /// <returns><b>true</b> if the specified objects are equal to each other; otherwise, <b>false</b>.</returns>
+        public static bool operator ==(Identifier left, Identifier right)
+        {
+            if (ReferenceEquals(left, null))
+                return ReferenceEquals(right, null);
+
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are not equal to each other.
+        /// </summary>
+        /// <param name="left">The first object to be compared.</param>
+        /// <param name="right">The second object to be compared.</param>
+        /// <returns><b>true</b> if the specified objects are equal to each other; otherwise, <b>false</b>.</returns>
+        public static bool operator !=(Identifier left, Identifier right)
+        {
+            if (ReferenceEquals(left, null))
+                return !ReferenceEquals(right, null);
+
+            return !left.Equals(right);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><b>true</b> if the specified object is equal to the current object; otherwise, <b>false</b>.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null))
+                return false;
+
+            return Equals(obj as Identifier);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="other">The object to compare with the current object.</param>
+        /// <returns><b>true</b> if the specified object is equal to the current object; otherwise, <b>false</b>.</returns>
+        public bool Equals(Identifier other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (!ReferenceEquals(TypeToResolve, other.TypeToResolve))
+                return false;
+
+            if (ReferenceEquals(Label, null))
+                return ReferenceEquals(other.Label, null);
+
+            return Label.Equals(other.Label, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = TypeToResolve.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Label == null ? 0 : Label.GetHashCode());
+                return hashCode;
+            }
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var labelSuffix = Label == null ? null : $" ({Label})";
+            return TypeToResolve.FullName + labelSuffix;
+        }
+
+        private static string GetTrimmedValueOrNull(string value)
+        {
+            if (value == null)
+                return null;
+
+            value = value.Trim();
+            if (value.Length == 0)
+                return null;
+
+            return value;
+        }
+    }
+}
---
+++ ContractorTests/Internal/IdentifierTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace Contractor.Internal
+{
+    public class IdentifierTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenTypeIsNull()
+        {
+            // Act
+            Action action = () => new Identifier(null);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>();
+        }
+        [Fact]
+        public void Constructor_ShouldStoreType()
+        {
+            // Arrange
+            var type = typeof(string);
+
+            // Act
+            var result = new Identifier(type);
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.TypeToResolve.ShouldBeSameAs(type);
+            result.Label.ShouldBeNull();
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", null)]
+        [InlineData("   ", null)]
+        [InlineData("bob", "bob")]
+        [InlineData("  bob  ", "bob")]
+        public void Constructor_ShouldStoreTypeAndLabel(string givenLabel, string expectedLabel)
+        {
+            // Arrange
+            var type = typeof(string);
+
+            // Act
+            var result = new Identifier(type, givenLabel);
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.TypeToResolve.ShouldBeSameAs(type);
+            result.Label.ShouldBe(expectedLabel);
+        }
+
+        //[Theory]
+        //[InlineData(true, true, true)]
+        //[InlineData(false, true, false)]
+        //[InlineData(true, false, false)]
+        //public void EqualityOperators_ShouldHandleNullValues(bool leftIsNull, bool rightIsNull, bool expectedResult)
+        //{
+        //    // Arrange
+        //    var left = leftIsNull ? null : new Identifier(typeof(int));
+        //    var right = rightIsNull ? null : new Identifier(typeof(string));
+
+        //    // Act
+        //    var equalityResult = left == right;
+        //    var inequalityResult = left != right;
+
+        //    // Assert
+        //    equalityResult.ShouldBe(expectedResult);
+        //    inequalityResult.ShouldNotBe(expectedResult);
+        //}
+
+        //[Theory]
+        //[InlineData(typeof(string), null, typeof(string), null, true)]
+        //[InlineData(typeof(string), null, typeof(string), "bob", false)]
+        //[InlineData(typeof(string), "bob", typeof(string), null, false)]
+        //[InlineData(typeof(string), "bob", typeof(string), "bob", true)]
+        //[InlineData(typeof(string), null, typeof(int), null, false)]
+        //[InlineData(typeof(string), null, typeof(int), "bob", false)]
+        //[InlineData(typeof(string), "bob", typeof(int), null, false)]
+        //[InlineData(typeof(string), "bob", typeof(int), "bob", false)]
+        //[InlineData(typeof(int), null, typeof(string), null, false)]
+        //[InlineData(typeof(int), null, typeof(string), "bob", false)]
+        //[InlineData(typeof(int), "bob", typeof(string), null, false)]
+        //[InlineData(typeof(int), "bob", typeof(string), "bob", false)]
+        //public void EqualityOperators_ShouldCompareValues(Type leftType, string leftLabel, Type rightType, string rightLabel, bool expectedResult)
+        //{
+        //    // Arrange
+        //    var left = new Identifier(leftType, leftLabel);
+        //    var right = new Identifier(rightType, rightLabel);
+
+        //    // Act
+        //    var equalityResult = left == right;
+        //    var inequalityResult = left != right;
+
+        //    // Assert
+        //    equalityResult.ShouldBe(expectedResult);
+        //    inequalityResult.ShouldNotBe(expectedResult);
+        //}
+
+        [Fact]
+        public void Equals_ShouldReturnFalse_WhenOtherIsWrongType()
+        {
+            // Arrange
+            var subject = new Identifier(typeof(int));
+
+            // Act
+            var result = subject.Equals("not an Identifier");
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Equals_ShouldReturnFalse_WhenOtherIsNull_AndTypeIsNotKnown()
+        {
+            // Arrange
+            var subject = new Identifier(typeof(int));
+
+            // Act
+            var result = subject.Equals((object)null);
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Equals_ShouldReturnFalse_WhenOtherIsNull()
+        {
+            // Arrange
+            var subject = new Identifier(typeof(int));
+
+            // Act
+            var result = subject.Equals(null);
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+
+        [Theory]
+        [InlineData(typeof(string), null, typeof(string), null, true)]
+        [InlineData(typeof(string), null, typeof(string), "bob", false)]
+        [InlineData(typeof(string), "bob", typeof(string), null, false)]
+        [InlineData(typeof(string), "bob", typeof(string), "bob", true)]
+        [InlineData(typeof(string), null, typeof(int), null, false)]
+        [InlineData(typeof(string), null, typeof(int), "bob", false)]
+        [InlineData(typeof(string), "bob", typeof(int), null, false)]
+        [InlineData(typeof(string), "bob", typeof(int), "bob", false)]
+        [InlineData(typeof(int), null, typeof(string), null, false)]
+        [InlineData(typeof(int), null, typeof(string), "bob", false)]
+        [InlineData(typeof(int), "bob", typeof(string), null, false)]
+        [InlineData(typeof(int), "bob", typeof(string), "bob", false)]
+        public void Equals_ShouldCompareValues(Type subjectType, string subjectLabel, Type otherType, string otherLabel, bool expectedResult)
+        {
+            // Arrange
+            var subject = new Identifier(subjectType, subjectLabel);
+            var other = new Identifier(otherType, otherLabel);
+
+            // Act
+            var typedResult = subject.Equals(other);
+            var untypedResult = subject.Equals((object)other);
+
+            // Assert
+            typedResult.ShouldBe(expectedResult);
+            untypedResult.ShouldBe(expectedResult);
+        }
+
+        public void GetHashCode_ShouldReturnUniqueValues()
+        {
+            // Arrange
+            var subject1 = new Identifier(typeof(string));
+            var subject2 = new Identifier(typeof(string), "bob");
+            var subject3 = new Identifier(typeof(int));
+            var subject4 = new Identifier(typeof(int), "bob");
+
+            // Act
+            var result1 = subject1.GetHashCode();
+            var result2 = subject2.GetHashCode();
+            var result3 = subject3.GetHashCode();
+            var result4 = subject4.GetHashCode();
+
+            // Assert
+            result1.ShouldNotBe(0);
+            result2.ShouldNotBe(0);
+            result3.ShouldNotBe(0);
+            result4.ShouldNotBe(0);
+
+            result1.ShouldNotBe(result2);
+            result1.ShouldNotBe(result3);
+            result1.ShouldNotBe(result4);
+
+            result2.ShouldNotBe(result3);
+            result2.ShouldNotBe(result4);
+
+            result3.ShouldNotBe(result4);
+        }
+
+        [Theory]
+        [InlineData(typeof(string), null, "System.String")]
+        [InlineData(typeof(string), "bob", "System.String (bob)")]
+        public void ToString_ShouldReturnFormattedString(Type type, string label, string expectedResult)
+        {
+            // Arrange
+            var subject = new Identifier(type, label);
+
+            // Act
+            var result = subject.ToString();
+
+            // Assert
+            result.ShouldBe(expectedResult);
+        }
+    }
+}

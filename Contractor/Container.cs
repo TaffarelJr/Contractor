f62b373538a3d981357ce8912a11715bd65f78ce
RJ Hollberg <rj.hollberg@outlook.com>
2017-01-30 23:50:57 -0700

    Add fluent extensions for creating Identifiers. This will help the container syntax to be more human-readable. Also made some previous tests a little more reliable.

--- Contractor/Container.cs
+++ Contractor/Container.cs
@@ -25,7 +25,7 @@ namespace Contractor
         /// <exception cref="ArgumentNullException"><paramref name="identifier"/> is <b>null</b>.
         /// -or- <paramref name="factory"/> is <b>null</b>.</exception>
         /// <exception cref="InvalidOperationException">The specified identifier is already registered with the container.</exception>
-        /// <exception cref="OverflowException">The container already contains the maximum number of registry entries (2,147,483,647).</exception>
+        /// <exception cref="OverflowException">The container already contains the maximum number of registry entries (<see cref="int.MaxValue"/>).</exception>
         public void Register(Identifier identifier, Factory factory)
         {
             if (identifier == null)
@@ -38,6 +38,52 @@ namespace Contractor
                 var labelString = identifier.Label == null ? null : $" with label '{identifier.Label}'";
                 throw new InvalidOperationException($"A registry entry already exists for type '{identifier.TypeToResolve.FullName}'{labelString}");
             }
+        }
+
+        /// <summary>
+        /// Registers the specified type to be resolved by the container.
+        /// </summary>
+        /// <typeparam name="T">The type to be resolved by the container.</typeparam>
+        /// <returns>A tuple containing the current <see cref="Container"/> and the new
+        /// <see cref="Identifier"/> that represents to type to be resolved by the container.</returns>
+        public (Container Container, Identifier Identifier) WhenResolving<T>()
+        {
+            return WhenResolving(typeof(T), null);
+        }
+
+        /// <summary>
+        /// Registers the specified type to be resolved by the container.
+        /// </summary>
+        /// <typeparam name="T">The type to be resolved by the container.</typeparam>
+        /// <param name="label">The optional label for the type to be resolved.</param>
+        /// <returns>A tuple containing the current <see cref="Container"/> and the new
+        /// <see cref="Identifier"/> that represents to type to be resolved by the container.</returns>
+        public (Container Container, Identifier Identifier) WhenResolving<T>(string label)
+        {
+            return WhenResolving(typeof(T), label);
+        }
+
+        /// <summary>
+        /// Registers the specified type to be resolved by the container.
+        /// </summary>
+        /// <param name="typeToResolve">The type to be resolved by the container.</param>
+        /// <returns>A tuple containing the current <see cref="Container"/> and the new
+        /// <see cref="Identifier"/> that represents to type to be resolved by the container.</returns>
+        public (Container Container, Identifier Identifier) WhenResolving(Type typeToResolve)
+        {
+            return WhenResolving(typeToResolve, null);
+        }
+
+        /// <summary>
+        /// Registers the specified type to be resolved by the container.
+        /// </summary>
+        /// <param name="typeToResolve">The type to be resolved by the container.</param>
+        /// <param name="label">The optional label for the type to be resolved.</param>
+        /// <returns>A tuple containing the current <see cref="Container"/> and the new
+        /// <see cref="Identifier"/> that represents to type to be resolved by the container.</returns>
+        public (Container Container, Identifier Identifier) WhenResolving(Type typeToResolve, string label)
+        {
+            return (this, new Identifier(typeToResolve, label));
         }
     }
 }
--- ContractorTests/ContainerTests.cs
+++ ContractorTests/ContainerTests.cs
@@ -42,12 +42,18 @@ namespace Contractor
             subject.Registry.ShouldNotBeNull();
             subject.Registry.Count.ShouldBe(2);
 
-            var registryItem = subject.Registry.First();
-            registryItem.Key.ShouldBeSameAs(identifier1);
+            var registryItem = subject.Registry
+                .Where(pair => pair.Key == identifier1)
+                .SingleOrDefault();
+
+            registryItem.ShouldNotBeNull();
             registryItem.Value.ShouldBeSameAs(factory);
 
-            registryItem = subject.Registry.Last();
-            registryItem.Key.ShouldBeSameAs(identifier2);
+            registryItem = subject.Registry
+                .Where(pair => pair.Key == identifier2)
+                .SingleOrDefault();
+
+            registryItem.ShouldNotBeNull();
             registryItem.Value.ShouldBeSameAs(factory);
         }
 
@@ -66,6 +72,53 @@ namespace Contractor
 
             // Assert
             action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void WhenResolving_ShouldRegisterTypes()
+        {
+            // Arrange
+            var label = "bob";
+            var type = typeof(string);
+            var subject = new Container();
+
+            // Act
+            var genericTuple = subject.WhenResolving(type);
+            var labeledTuple = subject.WhenResolving(type, label);
+
+            // Assert
+            genericTuple.Container.ShouldBeSameAs(subject);
+            genericTuple.Identifier.ShouldNotBeNull();
+            genericTuple.Identifier.TypeToResolve.ShouldBeSameAs(type);
+            genericTuple.Identifier.Label.ShouldBeNull();
+
+            labeledTuple.Container.ShouldBeSameAs(subject);
+            labeledTuple.Identifier.ShouldNotBeNull();
+            labeledTuple.Identifier.TypeToResolve.ShouldBeSameAs(type);
+            labeledTuple.Identifier.Label.ShouldBe(label);
+        }
+
+        [Fact]
+        public void WhenResolving_ShouldRegisterGenericTypes()
+        {
+            // Arrange
+            var label = "bob";
+            var subject = new Container();
+
+            // Act
+            var genericTuple = subject.WhenResolving<string>();
+            var labeledTuple = subject.WhenResolving<string>(label);
+
+            // Assert
+            genericTuple.Container.ShouldBeSameAs(subject);
+            genericTuple.Identifier.ShouldNotBeNull();
+            genericTuple.Identifier.TypeToResolve.ShouldBe(typeof(string));
+            genericTuple.Identifier.Label.ShouldBeNull();
+
+            labeledTuple.Container.ShouldBeSameAs(subject);
+            labeledTuple.Identifier.ShouldNotBeNull();
+            labeledTuple.Identifier.TypeToResolve.ShouldBe(typeof(string));
+            labeledTuple.Identifier.Label.ShouldBe(label);
         }
     }
 }

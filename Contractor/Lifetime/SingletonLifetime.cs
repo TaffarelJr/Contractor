d7925b49d3a18031254fb6cc8da1e9535e6b246b
RJ Hollberg <rj.hollberg@outlook.com>
2017-01-31 11:21:53 -0700

    Add SingletonLifetime. And unit tests.

---
+++ Contractor/Lifetime/SingletonLifetime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Contractor.Factory;
+
+namespace Contractor.Lifetime
+{
+    /// <summary>
+    /// An <see cref="ILifetime"/> object that creates a single instance and caches it.
+    /// </summary>
+    public class SingletonLifetime : BaseLifetime
+    {
+        private object _instance;
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="implementationType">The type to be constructed.</param>
+        /// <param name="factory">The <see cref="IFactory"/> to be used to construct the instance.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="implementationType"/> is <b>null</b>.
+        /// -or- <paramref name="factory"/> is <b>null</b>.</exception>
+        public SingletonLifetime(Type implementationType, IFactory factory)
+            : base(implementationType, factory)
+        {
+        }
+
+        /// <summary>
+        /// Instructs <see cref="BaseLifetime.Factory"/> to construct a single instance,
+        /// and caches it for future calls.
+        /// </summary>
+        /// <returns>The singleton instance of <see cref="BaseLifetime.ImplementationType"/>.</returns>
+        public override object GetInstance()
+        {
+            if (_instance == null)
+                _instance = Factory.ConstructNewInstance();
+
+            return _instance;
+        }
+    }
+}
---
+++ ContractorTests/Lifetime/SingletonLifetimeTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Contractor.Factory;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Contractor.Lifetime
+{
+    public class SingletonLifetimeTests
+    {
+        [Fact]
+        public void Constructor_ShouldValidateParameters()
+        {
+            // Arrange
+            var type = typeof(string);
+            var mockFactory = new Mock<IFactory>(MockBehavior.Strict);
+
+            // Act
+            Action action1 = () => new SingletonLifetime(null, null);
+            Action action2 = () => new SingletonLifetime(type, null);
+            Action action3 = () => new SingletonLifetime(null, mockFactory.Object);
+
+            // Assert
+            action1.ShouldThrow<ArgumentNullException>();
+            action2.ShouldThrow<ArgumentNullException>();
+            action3.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Constructor_ShouldStoreParameters()
+        {
+            // Arrange
+            var type = typeof(string);
+            var mockFactory = new Mock<IFactory>(MockBehavior.Strict);
+
+            // Act
+            var result = new SingletonLifetime(type, mockFactory.Object);
+
+            // Assert
+            result.ImplementationType.ShouldBeSameAs(type);
+            result.Factory.ShouldBeSameAs(mockFactory.Object);
+        }
+
+        [Fact]
+        public void Factory_ShouldThrowException_WhenValueIsNull()
+        {
+            // Arrange
+            var type = typeof(string);
+            var mockFactory = new Mock<IFactory>(MockBehavior.Strict);
+            var subject = new SingletonLifetime(type, mockFactory.Object);
+
+            // Act
+            Action action = () => subject.Factory = null;
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Factory_ShouldStoreNewValue()
+        {
+            // Arrange
+            var type = typeof(string);
+            var mockFactory1 = new Mock<IFactory>(MockBehavior.Strict);
+            var mockFactory2 = new Mock<IFactory>(MockBehavior.Strict);
+            var subject = new SingletonLifetime(type, mockFactory1.Object);
+
+            // Act
+            subject.Factory = mockFactory2.Object;
+
+            // Assert
+            subject.Factory.ShouldBeSameAs(mockFactory2.Object);
+        }
+
+        [Fact]
+        public void GetInstance_ShouldConstructSingleInstance()
+        {
+            // Arrange
+            var type = typeof(string);
+            var instances = new[] { "hello", "goodbye" };
+            var count = 0;
+
+            var mockFactory = new Mock<IFactory>(MockBehavior.Strict);
+            mockFactory
+                .Setup(f => f.ConstructNewInstance())
+                .Returns(instances[count++]);
+
+            var subject = new SingletonLifetime(type, mockFactory.Object);
+
+            // Act
+            var result1 = subject.GetInstance();
+            var result2 = subject.GetInstance();
+            var result3 = subject.GetInstance();
+
+            // Assert
+            result1.ShouldBe(instances[0]);
+            result2.ShouldBe(instances[0]);
+            result3.ShouldBe(instances[0]);
+
+            mockFactory.VerifyAll();
+        }
+    }
+}

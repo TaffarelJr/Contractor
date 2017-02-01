0df9fbd78c44ef8626726eb72bfee7302583d9d6
RJ Hollberg <rj.hollberg@outlook.com>
2017-02-01 09:25:22 -0700

    Inject DataService into ValuesController.

--- WebApplication/Controllers/ValuesController.cs
+++ WebApplication/Controllers/ValuesController.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Services;
 
 namespace WebApplication.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly IDataService _dataService;
+
+        public ValuesController(IDataService dataService)
+        {
+            _dataService = dataService ?? throw new ArgumentNullException(nameof(dataService));
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _dataService.SelectAll();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return _dataService.SelectById(id);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            _dataService.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            _dataService.Insert(id, value);
         }
 
         // DELETE api/values/5

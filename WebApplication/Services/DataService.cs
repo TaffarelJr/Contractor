f8f7455751eeb64d9691a30ca3e8d58f8c153742
RJ Hollberg <rj.hollberg@outlook.com>
2017-02-01 09:08:22 -0700

    Add injectable DataService.

---
+++ WebApplication/Services/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace WebApplication.Services
+{
+    public class DataService : IDataService
+    {
+        private readonly ConcurrentDictionary<int, string> _data = new ConcurrentDictionary<int, string>();
+
+        public DataService()
+        {
+            _data.TryAdd(1, "value1");
+            _data.TryAdd(2, "value2");
+        }
+
+        public IEnumerable<string> SelectAll()
+        {
+            return _data.Values;
+        }
+
+        public string SelectById(int id)
+        {
+            if (_data.TryGetValue(id, out string value))
+                return value;
+
+            return "<no data found>";
+        }
+
+        public void Add(string value)
+        {
+            var id = 0;
+            while (!_data.TryAdd(id++, value)) { };
+        }
+
+        public void Insert(int id, string value)
+        {
+            _data.AddOrUpdate(id, value, (i, v) => value);
+        }
+
+    }
+}
---
+++ WebApplication/Services/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication.Services
+{
+    public interface IDataService
+    {
+        IEnumerable<string> SelectAll();
+
+        string SelectById(int id);
+
+        void Add(string value);
+
+        void Insert(int id, string value);
+
+    }
+}
